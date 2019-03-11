fcb44dedd339e327ed9d3f2bce1cff766843f9dd
Owais Abukreifeh <me@owaismajid.com>
2019-03-11 03:00:58 +0200

    CLI

--- console.php
+++ console.php
@@ -1,8 +1,14 @@
 <?php
+require_once("./controllers/LevenshtienController.php");
+
 if(!defined("STDIN")) define("STDIN", fopen("php://stdin", "rb"));
 echo "Edit Distance Calculator (Levenshtein Method)" . PHP_EOL;
 echo str_repeat("*", 32) . PHP_EOL;
-echo "Please provide the two strings separated by comma [,]" . PHP_EOL;
-$line = fread(STDIN, 256);
-echo $line;
-
+echo "Please provide the first string" . PHP_EOL;
+echo ">";
+$str1 = fread(STDIN, 256);
+echo "Please provide the second string" . PHP_EOL;
+echo ">";
+$str2 = fread(STDIN, 256);
+echo "Levenshtein Distance is: " . levenshtein_dist($str1, $str2);
+echo PHP_EOL;

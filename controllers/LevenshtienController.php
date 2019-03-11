dbd237465db17aab87542e277427b0e6cabfe098
Owais Abukreifeh <me@owaismajid.com>
2019-03-11 03:00:44 +0200

    updated the including path

--- controllers/LevenshtienController.php
+++ controllers/LevenshtienController.php
@@ -1,5 +1,5 @@
 <?php
-require_once( "./classes/Levenshtein.php");
+require_once( __DIR__ . "/../classes/Levenshtein.php");
 
 function levenshtein_dist(string $str1, string $str2): int{
     $levenshtein = new Levenshtein($str1, $str2);

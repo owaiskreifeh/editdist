fa3e37cacbffbbf3c5bfa0a070b435770e15578b
Owais Abukreifeh <me@owaismajid.com>
2019-03-12 18:57:36 +0200

    not needed

--- controllers/HammingController.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once( "./classes/Hamming.php");
-
-function hamming_dist(string $str1, string $str2): int{
-    $hamming = new Hamming($str1, $str2);
-    return $hamming->getCount();
-}
--- controllers/LevenshtienController.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once( __DIR__ . "/../classes/Levenshtein.php");
-
-function levenshtein_dist(string $str1, string $str2): int{
-    $levenshtein = new Levenshtein($str1, $str2);
-    return $levenshtein->getCount();
-}
-
-

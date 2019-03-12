3d3a56612b88e73de3a1223cea0a12a07a8ca206
Owais Abukreifeh <me@owaismajid.com>
2019-03-12 18:55:05 +0200

    code reorder

--- console.php
+++ console.php
@@ -1,7 +1,8 @@
 <?php
+if(!defined("STDIN")) define("STDIN", fopen("php://stdin", "rb"));
+
 require_once("./controllers/LevenshtienController.php");
 
-if(!defined("STDIN")) define("STDIN", fopen("php://stdin", "rb"));
 echo "Edit Distance Calculator (Levenshtein Method)" . PHP_EOL;
 echo str_repeat("*", 32) . PHP_EOL;
 echo "Please provide the first string" . PHP_EOL;
